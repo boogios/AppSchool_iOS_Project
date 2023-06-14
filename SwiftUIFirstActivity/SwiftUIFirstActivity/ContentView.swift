099ac5f60f5994a548630ed9b288e18879f5c55b
tkddn0518 <tkddn0518@naver.com>
2023-06-14 18:07:57 +0900

    배열안에 딕셔너리로 name, nickname 설정해서 화면에 보여주도록 수정

--- SwiftUIFirstActivity/SwiftUIFirstActivity/ContentView.swift
+++ SwiftUIFirstActivity/SwiftUIFirstActivity/ContentView.swift
@@ -1,21 +1,74 @@
 import SwiftUI
 import AVFoundation
 
+let members: [[String: String]] = [
+    [
+        "name" : "suji",
+        "nickname" : "jangsupungdengE"
+    ],
+    [
+        "name" : "haeun",
+        "nickname" : "hani"
+    ],
+    [
+        "name" : "sangwoo",
+        "nickname" : "sanguh"
+    ],
+    [
+        "name" : "sungeun",
+        "nickname" : "womensilver"
+    ],
+    [
+        "name" : "soyeon",
+        "nickname" : "chicorita"
+    ],
+    [
+        "name" : "minseok",
+        "nickname" : "mindol"
+    ],
+    [
+        "name" : "sojung",
+        "nickname" : "chaeso"
+    ],
+]
+
 struct ContentView: View {
     
-    @State var name: String = ""
-    @State var nickname: String = ""
+//    @State var name: String = ""
+//    @State var nickname: String = ""
     @State var message: String = ""
     let speechSynth = AVSpeechSynthesizer()
+    
+    @State var selectedIndex: Int = 0
+    
+    var name: String {
+        getName(selectedIndex)
+    }
+    
+    var nickname: String {
+        getNickname(selectedIndex)
+    }
     
     var body: some View {
         VStack {
             Group {
-                Button("수지") {
-                    sayHello("suji", "jangsupungdengE")
+                // 버튼은 액션과 label을 각각
+                // 클로저로 받을 수 있기 때문에
+                // 아래와 같이 간략화된 모양으로 쓸 수 있음
+                Button {
+                    // action
+//                    sayHello("suji", "jangsupungdengE")
+                    selectedIndex = 0
+                } label: {
+                    // label
+                    Text("\(getName(0))")
                 }
+//                Button("수지") {
+//                    sayHello("suji", "jangsupungdengE")
+//                }
                 Button("하은") {
-                    sayHello("haeun", "hani")
+//                    sayHello("haeun", "hani")
+                    selectedIndex = 1
                 }
                 Button("상우") {
                     sayHello("sangwoo", "sanguh")
@@ -32,23 +85,45 @@ struct ContentView: View {
                 Button("소정") {
                     sayHello("sojung", "chaeso")
                 }
-                Text("\(message)")
+//                Text("\(message)")
+                Text("\(name), \(nickname)")
             }
             .padding()
+            
+//            TextField("Input your nickName", text: $textFieldText)
         }
         .font(.largeTitle)
         .padding()
     }
     
     func sayHello(_ name: String, _ nickname: String) {
-        self.name = name
-        self.nickname = nickname
-        message = "Hello \($name.wrappedValue) \($nickname.wrappedValue)"
+        // $를 붙이는 상황은 타입이 Binding<String>일 때 씀
+        // self.name = name
+        // self.nickname = nickname
+        // message = "Hello \($name.wrappedValue) \($nickname.wrappedValue)"
+        
+        message = "Hello \(name), \(nickname)"
         
         print(message)
         
         let utterance = AVSpeechUtterance(string: message)
         speechSynth.speak(utterance)
+    }
+    
+    func getName(_ index: Int) -> String {
+        
+        let member = members[index]
+        
+        if let name = member["name"] {
+            return name
+        }
+        // let name = member["name"] ?? ""
+        return ""
+    }
+    
+    func getNickname(_ index: Int) -> String {
+        let member = members[index]
+        return member["nickname"] ?? ""
     }
     
 }
